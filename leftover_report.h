fa92b327c3e7960025642314381c5bf5cb98e2b5
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-22 02:10:50 -0700

    first commit, implemented LeftoverReport class

--- leftover_report.h
+++ leftover_report.h
@@ -1,12 +1,17 @@
 #ifndef LEFTOVER_REPORT_H_
 #define LEFTOVER_REPORT_H_
 
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "leftover_record.h"
+
+// Wen Fan
+// CPSC 121L - 11
+// May 03, 2024
+// WenFan@csu.fullerton.edu
+// @Wen-qqi
 
 class LeftoverReport {
     // ======================= YOUR CODE HERE =======================
@@ -18,6 +23,25 @@ class LeftoverReport {
      // Pass objects by const reference when appropriate.
      // Remember that std::string is an object!
      // ===============================================================
-};
+ public:
+  LeftoverReport();
+  LeftoverReport(const std::vector<LeftoverRecord> &leftover_records)
+                 : leftover_records_(leftover_records) {}
 
+  std::vector<std::string> GetMostCommonLeftover() const;
+  std::vector<std::string> GetMostCostlyLeftoverProducingMeals() const;
+  double GetTotalCostOfLeftover() const;
+  std::vector<std::string> GetMostCommonLeftoverReasons() const;
+  std::vector<std::string> GetMostCommonDisposalMechanisms() const;
+  std::vector<std::string> GetSuggestedLeftoverReductionStrategies() const;
+
+ private:
+  const std::vector<LeftoverRecord> &leftover_records_;
+  std::vector<std::string> most_common_leftover_;
+  std::vector<std::string> most_costly_leftover_producing_meals_;
+  double total_cost_of_leftover_;
+  std::vector<std::string> most_common_leftover_reasons_;
+  std::vector<std::string> most_common_disposal_mechanisms_;
+  std::vector<std::string> suggested_strategies_to_reduce_leftovers_;
+};
 #endif
