25babf0418846d5b5f5431694d058d337cdf7e15
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-22 02:14:09 -0700

    first commit, finished leftovertracker class, did not do delete

--- leftover_tracker.h
+++ leftover_tracker.h
@@ -1,13 +1,17 @@
 #ifndef LEFTOVER_TRACKER_H_
 #define LEFTOVER_TRACKER_H_
 
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Wen Fan
+// CPSC 121L - 11
+// May 03, 2024
+// WenFan@csu.fullerton.edu
+// @Wen-qqi
 
+#include <iostream>
+#include <vector>
+
+#include "leftover_record.h"
+#include "leftover_report.h"
 
 class LeftoverTracker {
     // ======================= YOUR CODE HERE =======================
@@ -19,6 +23,14 @@ class LeftoverTracker {
       // Pass objects by const reference when appropriate.
       // Remember that std::string is an object!
       // ===============================================================
+ public:
+  bool AddRecord(const LeftoverRecord& record);
+  bool DeleteRecord(const LeftoverRecord &record);
+  const std::vector<LeftoverRecord>& GetRecords() const;
+  LeftoverReport GetLeftoverReport() const;
+
+ private:
+  std::vector<LeftoverRecord> leftover_records_;
 };
 
 #endif
