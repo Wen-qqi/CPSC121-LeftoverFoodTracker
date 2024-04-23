8a801cd466570144711aaaec2ccd156362c08fbb
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-23 01:17:19 -0700

    second commit, fixed some format

--- leftover_record.cc
+++ leftover_record.cc
@@ -18,69 +18,40 @@
 // to tell the compiler that each function belongs to the LeftLeftoverRecord
 // class.
 // ===================================================================
+std::string LeftoverRecord::GetDate() const { return date_; }
 
-//std::string LeftoverRecord::GetDate() const { return date_;}
+void LeftoverRecord::SetDate(const std::string &date) { date_ = date; }
 
-// void LeftoverRecord::SetDate(const std::string& date) const { date_ = date; }
+std::string LeftoverRecord::GetMeal() const { return meal_; }
 
-//std::string LeftoverRecord::GetMeal() const{
-//   return meal_;
-// }
+void LeftoverRecord::SetMeal(const std::string &meal) { meal_ = meal; }
 
-//void LeftoverRecord::SetMeal(const std::string& meal) const {
-//    meal_ = meal;
-// }
+std::string LeftoverRecord::GetFoodName() const { return food_name_; }
 
-// std::string LeftoverRecord::GetFoodName() const {
-//    return food_name_;
-// }
+void LeftoverRecord::SetFoodName(const std::string &food_name) {
+    food_name_ = food_name;
+}
 
-// void LeftoverRecord::SetFoodName(const std::string& food_name) const {
-//     food_name_ = food_name;
-// }
+double LeftoverRecord::GetQuantityOunces() const {
+    return quantity_ounces_;
+}
 
-// double LeftoverRecord::GetQuantityOunces() const {
-//    return quantity_ounces_;
-// }
+void LeftoverRecord::SetQuantityOunces(double quantity_ounce) {
+    quantity_ounces_ = quantity_ounce;
+}
 
-// void LeftoverRecord::SetQuantityOunces(const double& quantity_ounce) const {
-//    quantity_ounces_ = quantity_ounce;
-// }
+std::string LeftoverRecord::GetLeftoverReason() const {
+    return leftover_reason_;
+}
+void LeftoverRecord::SetLeftoverReason(const std::string &leftover_reason) {
+    leftover_reason_ = leftover_reason;
+}
 
-// std::string LeftoverRecord::GetLeftoverReason() const {
-//    return leftover_reason_;
-// }
-
-// void LeftoverRecord::SetLeftoverReason(const std::string& leftover_reason) const {
-//    leftover_reason_ = leftover_reason;
-// }
-
-// std::string LeftoverRecord::GetDisposalMechanism() const {
-//    return disposal_mechanism_;
-// }
-
-// void LeftoverRecord::SetDisposalMechanism(const std::string& disposal_mechanism) const {
-//     disposal_mechanism_ = disposal_mechanism;
-// }
-
-// double LeftoverRecord::GetCost() const {
-//     return cost_;
-// }
-// void LeftoverRecord::SetCost(double cost) {
-//    cost_ = cost;
-// }
-
-bool LeftoverRecord::operator==(const LeftoverRecord &other) {
-    if (date_ == other.date_ && meal_ == other.meal_
-        && food_name_ == other.food_name_ &&
-        quantity_ounces_ == other.quantity_ounces_
-        && leftover_reason_ == other.leftover_reason_ &&
-        disposal_mechanism_ == other.disposal_mechanism_
-        && cost_ == other.cost_) {
-      return true;
-    } else {
-      return false;
-    }
+std::string LeftoverRecord::GetDisposalMechanism() const {
+    return disposal_mechanism_;
+}
+void LeftoverRecord::SetDisposalMechanism(const std::string &disposal_mechanism) {
+    disposal_mechanism_ = disposal_mechanism;
 }
 
 double LeftoverRecord::GetCost() const {
@@ -90,51 +61,14 @@ void LeftoverRecord::SetCost(double cost) {
     cost_ = cost;
 }
 
-std::string LeftoverRecord::GetDate() const {
-    return date_;
-}
-
-void LeftoverRecord::SetDate(const std::string &date) {
-    date_ = date;
-}
-
-// Meal
-std::string LeftoverRecord::GetMeal() const {
-    return meal_;
-}
-void LeftoverRecord::SetMeal(const std::string &meal) {
-    meal_ = meal;
-}
-
-  // Food Name
-std::string LeftoverRecord::GetFoodName() const {
-    return food_name_;
-}
-void LeftoverRecord::SetFoodName(const std::string &food_name) {
-    food_name_ = food_name;
-}
-
-  // Quantity Ounce
-double LeftoverRecord::GetQuantityOunces() const {
-    return quantity_ounces_;
-}
-
-void LeftoverRecord::SetQuantityOunces(double quantity_ounce) {
-    quantity_ounces_ = quantity_ounce;
-}
-
-  // Leftover Reason
-std::string LeftoverRecord::GetLeftoverReason() const {
-    return leftover_reason_;
-}
-void LeftoverRecord::SetLeftoverReason(const std::string &leftover_reason) {
-    leftover_reason_ = leftover_reason;
-}
-
-  // Disposal Mechanism
-std::string LeftoverRecord::GetDisposalMechanism() const {
-    return disposal_mechanism_;
-}
-void LeftoverRecord::SetDisposalMechanism(const std::string &disposal_mechanism) {
-    disposal_mechanism_ = disposal_mechanism;
+bool LeftoverRecord::operator==(const LeftoverRecord &other) {
+    if (date_ == other.date_ && meal_ == other.meal_ &&
+        food_name_ == other.food_name_ && quantity_ounces_ == other.quantity_ounces_ &&
+        leftover_reason_ == other.leftover_reason_ &&
+        disposal_mechanism_ == other.disposal_mechanism_ &&
+        cost_ == other.cost_) {
+            return true;
+        } else {
+            return false;
+        }
 }
